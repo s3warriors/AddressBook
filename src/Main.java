5ff9dca0730cb94bd3fda0cb7eba0f7fc525919e
s3warriors <satveerswami982@gmail.com>
2024-11-28 18:42:22 +0530

    added : add contacts functionality

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,49 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        AddressBook a1 = new AddressBook();
+        int i = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Address book");
+        do{
+            System.out.println("Enter 1 to create contact ");
+            int input = sc.nextInt();
+            switch (input){
+                case 1 -> createContact(a1);
+                default -> System.out.println("Wrong input");
+            }
+            System.out.println("Enter 0 to exit");
+            i = sc.nextInt();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        }while(i!=0);
+
+        System.out.println("Thank you for using address book");
+        sc.close();
+
+
+    }
+
+    static void createContact(AddressBook a1) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name");
+        String name = sc.next();
+        System.out.println("Enter last name");
+        String lname = sc.next();
+        System.out.println("Enter city");
+        String city = sc.next();
+        System.out.println("Enter state");
+        String state = sc.next();
+        System.out.println("Enter email");
+        String email = sc.next();
+        System.out.println("Enter phone");
+        int phone = sc.nextInt();
+        System.out.println("Enter name");
+        int zip = sc.nextInt();
+        Contact c1 = new Contact(name,lname,city,state,email,phone,zip);
+        a1.addContact(c1);
+
     }
 }
